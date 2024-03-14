d34d795ea6bc9f35a8ff1c05a764360a0aa781b8
gaozhe <851947523@qq.com>
2024-03-14 11:01:49 +0800

    更新文件上传本地

--- src/lib/files/types/Local.php
+++ src/lib/files/types/Local.php
@@ -2,6 +2,7 @@
 
 namespace Gz\TpCommon\lib\files\types;
 
+use Gz\TpCommon\exception\Error;
 use think\facade\Config;
 
 /**
@@ -12,14 +13,25 @@ use think\facade\Config;
  */
 class Local extends Base
 {
-    public function uploadSingle()
+    public function uploadSingle($validateType = ['file'=>'fileSize:10240|fileExt:jpg,png'])
     {
-        // 获取表单上传文件 例如上传了001.jpg
-        $file = request()->file($this->filename);
-        if (!$file) return;
-        // 上传到本地服务器
-        $savename = \think\facade\Filesystem::putFile('upload/' . $this->bucket, $file);
-        return '/' . $savename;
+        try {
+            // 获取表单上传文件 例如上传了001.jpg
+            $file = request()->file($this->filename);
+            validate($validateType)->check([$this->filename=>$file]);
+            if (!$file) return;
+             // 上传到本地服务器
+            $savename = \think\facade\Filesystem::putFile('upload/' . $this->bucket, $file);
+            return [
+                'path' => '/' . $savename,
+                'ext' => $file->getOriginalExtension(),
+                'hash' => $file->hash(),
+                'file_name'=>$file->getOriginalName(),
+            ];
+        } catch (\think\exception\ValidateException $e) {
+            throw new Error($e->getMessage());
+        }
+
     }
 
     public function uploadMore()
